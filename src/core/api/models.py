596686518bff635528f368fc2c3818933a646714
vaporoxx <jan.zichler@gmx.de>
2025-10-29 14:09:43 +0100

    feat: datenpunkte api

--- src/core/api/models.py
+++ src/core/api/models.py
@@ -3,35 +3,40 @@ import enum
 
 import pydantic
 
+from core.types import ErzeugerArt
 
+
+# Ein Datenpunkt eines Erzeugers
+class Erzeugnis(pydantic.BaseModel):
+	anfang: datetime.datetime
+	ende: datetime.datetime
+	wert: float
+
+
+# Ein Datenpunkt eines Ausbaupfades
 class Datenpunkt(pydantic.BaseModel):
-	art: str
+	erzeuger: ErzeugerArt
 	wert: float
 	zeitpunkt: datetime.datetime
 
 
 # Die Art eines Wetterereignisses
 class EreignisArt(enum.Enum):
-	Duerre = enum.auto()
-	Regen = enum.auto()
-	Schneefall = enum.auto()
-	Sturm = enum.auto()
+	Duerre = "Dürre"
+	Regen = "Regen"
+	Schneefall = "Schneefall"
+	Sturm = "Sturm"
 
 
 # Ein Wetterereignis eines Ausbaupfades
 class Ereignis(pydantic.BaseModel):
 	art: EreignisArt
 	ende: datetime.datetime
+	intensitaet: int
 	start: datetime.datetime
-	wert: int = pydantic.Field(ge=0, le=5)
 
 
-class Datenpunkt(pydantic.BaseModel):
-	anfang: datetime.datetime
-	ende: datetime.datetime
-	wert: float
-
-
-class Erzeuger(pydantic.BaseModel):
-	art: str
-	name: str
+# Ein Ausbaupfad eines Benutzers
+class Ausbaupfad(pydantic.BaseModel):
+	datenpunkte: list[Datenpunkt]
+	ereignisse: list[Ereignis]
--- src/core/api/routes.py
+++ src/core/api/routes.py
@@ -1,57 +1,84 @@
-import fastapi
-import pandas
+from datetime import datetime
 
-from core.api.models import DatenpunktModel, ErzeugerModel
+from fastapi import FastAPI, HTTPException
+from pandas import DataFrame
+
+from core.api.models import Ausbaupfad, Erzeugnis
 from core.erzeuger import ErzeugerArt
+from core.prognose import ausbaupfad, datenpunkt
 from core.setup.smard import Smard
 
 
-def datenpunkte(df: pandas.DataFrame) -> list[DatenpunktModel]:
+def erzeugnisse(
+	df: DataFrame, a: datetime | None = None, e: datetime | None = None
+) -> list[Erzeugnis]:
 	models = []
+
+	if a is not None:
+		df = df[a <= df["Datum von"]]
+
+	if e is not None:
+		df = df[e >= df["Datum von"]]
 
 	for _, row in df.iterrows():
 		anfang, ende, wert = row
-		models.append(DatenpunktModel(anfang=anfang, ende=ende, wert=wert))
+		models.append(Erzeugnis(anfang=anfang, ende=ende, wert=wert))
 
 	return models
 
 
-def setup(app: fastapi.FastAPI, smard: Smard) -> None:
+def setup(app: FastAPI, smard: Smard) -> None:
 	@app.get("/erzeuger")
-	def _() -> list[ErzeugerModel]:
-		models = []
-
-		for erzeuger in smard.erzeuger:
-			art = erzeuger.art.name
-			name = erzeuger.art.value
-
-			models.append(ErzeugerModel(art=art, name=name))
-
-		return models
+	def _() -> list[ErzeugerArt]:
+		return [erzeuger.art for erzeuger in smard.erzeuger]
 
 	@app.get("/erzeuger/{art}/installiert")
-	def _(art: str) -> list[DatenpunktModel]:
+	def _(
+		art: ErzeugerArt, anfang: datetime | None = None, ende: datetime | None = None
+	) -> list[Erzeugnis]:
 		try:
-			erzeuger = smard.get_erzeuger(ErzeugerArt[art])
+			erzeuger = smard.get_erzeuger(art)
 		except KeyError:
-			raise fastapi.HTTPException(404, "Erzeuger existiert nicht")
+			raise HTTPException(404, "Erzeuger existiert nicht")
 
-		return datenpunkte(erzeuger.installiert.df)
+		return erzeugnisse(erzeuger.installiert.df, anfang, ende)
 
 	@app.get("/erzeuger/{art}/normiert")
-	def _(art: str) -> list[DatenpunktModel]:
+	def _(
+		art: ErzeugerArt, anfang: datetime | None = None, ende: datetime | None = None
+	) -> list[Erzeugnis]:
 		try:
-			erzeuger = smard.get_erzeuger(ErzeugerArt[art])
+			erzeuger = smard.get_erzeuger(art)
 		except KeyError:
-			raise fastapi.HTTPException(404, "Erzeuger existiert nicht")
+			raise HTTPException(404, "Erzeuger existiert nicht")
 
-		return datenpunkte(erzeuger.normiert.df)
+		return erzeugnisse(erzeuger.normiert.df, anfang, ende)
 
 	@app.get("/erzeuger/{art}/realisiert")
-	def _(art: str) -> list[DatenpunktModel]:
+	def _(
+		art: ErzeugerArt, anfang: datetime | None = None, ende: datetime | None = None
+	) -> list[Erzeugnis]:
 		try:
-			erzeuger = smard.get_erzeuger(ErzeugerArt[art])
+			erzeuger = smard.get_erzeuger(art)
 		except KeyError:
-			raise fastapi.HTTPException(404, "Erzeuger existiert nicht")
+			raise HTTPException(404, "Erzeuger existiert nicht")
 
-		return datenpunkte(erzeuger.realisiert.df)
+		return erzeugnisse(erzeuger.realisiert.df, anfang, ende)
+
+	@app.post("/ausbaupfade")
+	def _(
+		pfad: Ausbaupfad, anfang: datetime | None = None, ende: datetime | None = None
+	) -> dict[ErzeugerArt, list[Erzeugnis]]:
+		datenpunkte = []
+
+		for dp in pfad.datenpunkte:
+			datenpunkte.append(
+				datenpunkt.Datenpunkt(art=dp.erzeuger, datetime=dp.zeitpunkt, installiert=dp.wert)
+			)
+
+		konstruiert = ausbaupfad.Ausbaupfad(datenpunkte, smard=smard)
+
+		d = {}
+		for dr in konstruiert.prognose_datenreihen:
+			d[dr.art] = erzeugnisse(dr.df, anfang, ende)
+		return d
